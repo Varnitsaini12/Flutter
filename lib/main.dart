9261b2ee0e8b5aaef63a9848932888c42376e524
Varnit Saini <varnitsaini2222@gamil.comm>
2023-06-17 05:29:22 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,19 +11,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name = "Varnit12";
-    double pi = 3.14;
-    bool isMale = true;
-    num temp = 30.5;
-
-    var day =
-        "Friday"; // compiler understand it without giving actual data type
-    const pi1 = 3.14;
-    // final  is also like const but we can later change the final but not in const
-
-    return const MaterialApp(
-      home: Homepage(),
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.amber,
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/home" : (contect)=> Homepage(),
+        "/Login" : (context)=>LoginPage(),
+      },
     );
   }
 }
