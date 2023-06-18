17651bb33b3faa42c3708defaa18bc3f875519ec
Varnit Saini <varnitsaini2222@gamil.comm>
2023-06-18 20:17:14 +0530

    themes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 import 'package:flutter_application_1/utils/routes.dart';
+import 'package:flutter_application_1/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -15,14 +18,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        primarySwatch: Colors.amber,
-        brightness: Brightness.dark,
-      ),
       initialRoute: MyRoutes.homeroute,
       routes: {
         MyRoutes.homeroute : (contect)=> Homepage(),
